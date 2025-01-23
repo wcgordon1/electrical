68bcdb9a2924518de4027a6e3b0ed3b55ca8a371
wcgordon1 <wcgordon1@gmail.com>
2025-01-23 05:57:36 -0800

    updated rss and am keeping engines becasue chatgpt said so

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import { marked } from 'marked';
 
 export const prerender = true;
 
@@ -12,32 +13,32 @@ export async function GET(context) {
     site: context.site,
     items: jobs.map((job) => ({
       title: job.data.position,
+      company: job.data.hiringOrganization.name,
+      companyLogo: job.data.hiringOrganization.logo,
+      companyUrl: job.data.hiringOrganization.sameAs,
       pubDate: new Date(job.data.datePosted),
       description: `
-        <div>
-          <img src="${job.data.hiringOrganization.logo}" alt="${job.data.hiringOrganization.name} logo" style="max-width: 200px; margin-bottom: 20px;" />
-          <h2>${job.data.position}</h2>
-          <p><strong>Company:</strong> ${job.data.hiringOrganization.name}</p>
-          <p><strong>Location:</strong> ${job.data.location}</p>
-          <p><strong>Salary Range:</strong> $${job.data.baseSalary.minValue} - $${job.data.baseSalary.maxValue} ${job.data.baseSalary.unitText}</p>
-          <p><strong>Job Type:</strong> ${job.data.employmentType}</p>
-          <p><strong>Remote:</strong> No</p>
-          <br/>
-          <p>${job.data.description}</p>
-        </div>
+          <div>${marked.parse(job.body)}</div>
       `,
       link: `${context.site}jobs/${job.slug}`,
       customData: `
-        <company>${job.data.hiringOrganization.name}</company>
-        <companyLogo>${job.data.hiringOrganization.logo}</companyLogo>
         <datePosted>${job.data.datePosted}</datePosted>
+        <validThrough>${job.data.validThrough}</validThrough>
         <location>${job.data.location}</location>
+        <streetAddress>${job.data.jobLocation.streetAddress}</streetAddress>
+        <city>${job.data.jobLocation.addressLocality}</city>
+        <state>${job.data.jobLocation.addressRegion}</state>
+        <country>${job.data.jobLocation.addressCountry}</country>
+        <postalCode>${job.data.jobLocation.postalCode}</postalCode>
         <salary>
           <currency>${job.data.baseSalary.currency}</currency>
           <minimum>${job.data.baseSalary.minValue}</minimum>
           <maximum>${job.data.baseSalary.maxValue}</maximum>
-          <unit>${job.data.baseSalary.unitText}</unit>
+          <monetaryAmount>${job.data.baseSalary.unitText}</monetaryAmount>
         </salary>
+        <category>${job.data.occupationalCategory}</category>
+        <team>${job.data.team}</team>
+        <setting>
         <remote>false</remote>
         <employmentType>${job.data.employmentType}</employmentType>
       `,
